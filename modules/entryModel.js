1e3c8d748c5a10550001162e51e42aa5d693b97f
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-04 19:00:56 -0700

    EventTracker modification

--- modules/entryModel.js
+++ modules/entryModel.js
@@ -8,30 +8,30 @@ const pool = new Pool({connectionString: db_url});
 function getLastEntryFromDB(callback) {
     console.log("Back from the getLastEntryFromDB");
     
-    var sql = "SELECT 
+    var sql = "SELECT entry_ID_PK, entry_content, entry_date, entry_timeline, entry_acct_FK FROM eventEntry WHERE entry_acct_fk = 1";
+    
+    pool.query(sql, function(errror, db_results) {
+        if (error || db_results == null || db_results.length < 1) {
+            response.status(500).json({success:false, data: error});
+        } else {
+            // entryID = results[results.length - 1].entry_id_pk;
+            // acct = results[results.length - 1].entry_acct_fk;
+            var results = {ID: db_results[db_results.length - 1].entry_id_pk,
+                            content: db_results[db_results.length - 1].entry_content,
+                            date: db_results[db_results.length - 1].entry_date,
+                            timeline: db_results[db_results.length - 1].entry_timeline,
+                            accountID: db_results[db_results.length - 1].entry_acct_fk
+                          };
         
-    if (error || results == null || results.length < 1) {
-        response.status(500).json({success:false, data: error});
-        
-    } else {
-        
-        entryID = results[results.length - 1].entry_id_pk;
-        acct = results[results.length - 1].entry_acct_fk;
-        params = {ID: results[results.length - 1].entry_id_pk,
-                      content: results[result.length - 1].entry_content,
-                      date: results[results.length - 1].entry_date,
-                      timeline: results[results.length - 1].entry_timeline,
-                      accountID: results[results.length - 1].entry_acct_fk};
-        
-        console.log("Transfered to params: ", params);
+            console.log("Transfered to results: ", results);
+            
+            callback(null, results);
         
         // response.writeHead(200, {"Content-Type": "application/json"});
         // response.render('lastEntry', params);
         // response.end();
-    }
-    
-    callback(null, params);    
-};
+    }    
+});
 
 module.exports = {
     getLastEntryFromDB: getLastEntryFromDB
