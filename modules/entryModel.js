a3090e17e53011224a0ca6e2909fe0413a0d81cc
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-04 17:56:48 -0700

    EventTracker modification

--- modules/entryModel.js
+++ modules/entryModel.js
@@ -1,19 +1,19 @@
 
 
 function getLastEntryFromDB(callback) {
-    console.log("Back from the getLastEntryFromDB function with result: ", result);
+    console.log("Back from the getLastEntryFromDB function with result: ", results);
         
     if (error || results == null || results.length < 1) {
         response.status(500).json({success:false, data: error});
     } else {
         // var params = JSON.stringify(result);
-        entryID = result[result.length - 1].entry_id_pk;
-        acct = result[result.length - 1].entry_acct_fk;
-        params = {ID: result[result.length - 1].entry_id_pk,
-                      content: result[result.length - 1].entry_content,
-                      date: result[result.length - 1].entry_date,
-                      timeline: result[result.length - 1].entry_timeline,
-                      accountID: result[result.length - 1].entry_acct_fk};
+        entryID = result[results.length - 1].entry_id_pk;
+        acct = result[results.length - 1].entry_acct_fk;
+        params = {ID: results[results.length - 1].entry_id_pk,
+                      content: results[result.length - 1].entry_content,
+                      date: results[results.length - 1].entry_date,
+                      timeline: results[results.length - 1].entry_timeline,
+                      accountID: results[results.length - 1].entry_acct_fk};
         
         console.log("Transfered to params: ", params);
         
