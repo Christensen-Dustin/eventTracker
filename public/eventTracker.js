df2bea8f595a49e073a781a5476fad44d684202a
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-05 00:10:14 -0700

    EventTracker Theme

--- public/eventTracker.js
+++ public/eventTracker.js
@@ -18,7 +18,7 @@ function loadUser() {
         console.log(data);
     
         var user = data.list[0];
-        entryID = parseInt(user.account_ID_PK);
+        entryID = parseInt(user.account_ID_PK) + user.account_ID_PK;
         
         $("#user").append("<b>Greetings: </b> " + user.account_name +
                               " --- <b>UserID: </b> " + parseInt(user.account_ID_PK));
