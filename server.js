4bf81d772b4d51b7cc8b8b5f82ceb393b14b0643
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-06 11:40:59 -0700

    EventTracker Update newNote

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extened: true}));
+app.use(express.urlencoded({extended: true}));
 
 // User Controller
 app.get("/getUser", userController.getUserData);
