d18389136df92e849c5a47a043e4707912aa942f
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-06 15:11:35 -0700

    EventTracker Update SESSION

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(session());
+// app.use(session());
 
-var userData;
+// var userData;
 
 // User Controller
 app.get("/getUser", userController.getUserData);
