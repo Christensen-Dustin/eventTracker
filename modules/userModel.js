d041e2ff50703b40a7271fadc9bc1a8990a3885c
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-05 22:07:51 -0700

    EventTracker update - Theme

--- modules/userModel.js
+++ modules/userModel.js
@@ -15,7 +15,7 @@ function getUserDataFromDB(id, callback) {
         if (error) {
             throw error;
         } else {
-            console.log("db_reults: " + db_results);
+            console.log("db_reults: " + JSON.stringify(db_results));
             
             var results = { success: true, list: db_results.rows };
         
