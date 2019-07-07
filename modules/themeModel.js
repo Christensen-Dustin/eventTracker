472e65f596fea246aff9ce5b5df08785b175b464
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-07 00:28:49 -0700

    EventTracker Update ThemeList

--- modules/themeModel.js
+++ modules/themeModel.js
@@ -32,7 +32,7 @@ function getThemesFromDB(id, entry, callback) {
 function getThemeListFromDB(id, callback) {
     console.log("Back from the getThemeListFromDB ID: " + id);
     
-    var sql = "SELECT theme_id_pk, theme_name, theme_acct_fk" +
+    var sql = "SELECT theme_name, theme_acct_fk, theme_id_pk" +
         "FROM eventTheme WHERE theme_acct_FK = $1::int";
     var params = [id];
     
